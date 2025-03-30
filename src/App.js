83da4f9a62bcc34f442909424d56120ee4e27342
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-29 22:52:53 -0400

    Tip Calculator

--- src/App.js
+++ src/App.js
@@ -10,14 +10,33 @@ export default function App() {
 
 function TipCalculator() {
   const [pay, setPay] = useState("");
+  const [tip1, setTip1] = useState(0);
+  const [tip2, setTip2] = useState(0);
+
+  const percentage = pay * ((tip1 + tip2) / 2 / 100);
+
+  function handleReset() {
+    setPay("");
+    setTip1(0);
+    setTip2(0);
+  }
 
   return (
     <div>
       <Payment pay={pay} onSetPay={setPay} />
-      <Tip>How did you like the service?</Tip>
-      <Tip>How did your friend like the service?</Tip>
-      <Output pay={pay} />
-      <Reset />
+      <Tip tip={tip1} onSelect={setTip1}>
+        How did you like the service?
+      </Tip>
+      <Tip tip={tip2} onSelect={setTip2}>
+        How did your friend like the service?
+      </Tip>
+
+      {pay > 0 && (
+        <>
+          <Output pay={pay} percentage={percentage} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
@@ -36,11 +55,11 @@ function Payment({ pay, onSetPay }) {
   );
 }
 
-function Tip({ children }) {
+function Tip({ children, tip, onSelect }) {
   return (
     <div>
       <label>{children}</label>
-      <select>
+      <select value={tip} onChange={(e) => onSelect(Number(e.target.value))}>
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -50,10 +69,14 @@ function Tip({ children }) {
   );
 }
 
-function Output({ pay }) {
-  return <h3>You pay X (${pay} + $B tip)</h3>;
+function Output({ pay, percentage }) {
+  return (
+    <h3>
+      You pay ${pay + percentage} (${pay} + ${percentage} tip)
+    </h3>
+  );
 }
 
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
