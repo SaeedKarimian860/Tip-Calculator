00921b46cdd3860ccebd225919fef06faf37e595
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-29 22:33:37 -0400

    Tip Calculator

--- src/App.js
+++ src/App.js
@@ -9,48 +9,51 @@ export default function App() {
 }
 
 function TipCalculator() {
+  const [pay, setPay] = useState("");
+
   return (
     <div>
-      <Payment />
+      <Payment pay={pay} onSetPay={setPay} />
       <Tip>How did you like the service?</Tip>
       <Tip>How did your friend like the service?</Tip>
-      <Output />
+      <Output pay={pay} />
       <Reset />
     </div>
   );
 }
 
-function Payment() {
-  return (
-    <div className="Payment">
-      <label>How much was the bill?</label>
-      <input type="text" placeholder="Bill value" />
-    </div>
-  );
-}
-
-function Tip({children}) {
+function Payment({ pay, onSetPay }) {
   return (
     <div>
-        <label>{children}</label>
-     <select>
-        <option value='0'>Dissatisfied (0%)</option>
-        <option value='5'>It was okay (5%)</option>
-        <option value='10'>It was good (10%)</option>
-        <option value='20'>Absolutely amazing! (20%)</option>
-     </select>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={pay}
+        onChange={(e) => onSetPay(Number(e.target.value))}
+      />
     </div>
   );
 }
 
-function Output() {
-    return (
-        <h3>You pay X ($A + $B tip)</h3>
-    )
+function Tip({ children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output({ pay }) {
+  return <h3>You pay X (${pay} + $B tip)</h3>;
 }
 
 function Reset() {
-    return (
-        <button>Reset</button>
-    )
+  return <button>Reset</button>;
 }
